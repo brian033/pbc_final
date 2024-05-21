e0524790cee5da42c614644c9df63231549d0025
brian033 <85883730+brian033@users.noreply.github.com>
2024-05-21 21:52:46 +0800

    finished main.py

--- main.py
+++ main.py
@@ -20,5 +20,30 @@ for user in userArray:
             compatibleUsers.append((otherUser.id, dist))
     compatibleUsers.sort(key=lambda x: x[1])
     compatibleLst.append(compatibleUsers)
+
+result = list()
 for i in range(len(compatibleLst)):
-    print("User", i, " top 3 matches:", compatibleLst[i][:3])
+    userInfo = userArray[i].get_user_info()
+    first = userArray[compatibleLst[i][0][0]]
+    second = userArray[compatibleLst[i][1][0]]
+    third = userArray[compatibleLst[i][2][0]]
+    res = {
+        "id": i,
+        "name": userInfo["name"],
+        "introduction": userInfo["introduction"],
+        "email":  userInfo["email"],
+        "id_first": compatibleLst[i][0][0],
+        "name_first": first.get_user_info()["name"],
+        "intro_first": first.get_user_info()["introduction"],
+        "email_first": first.get_user_info()["email"],
+        "id_second": compatibleLst[i][1][0],
+        "name_second": second.get_user_info()["name"],
+        "intro_second": second.get_user_info()["introduction"],
+        "email_second": second.get_user_info()["email"],
+        "id_third": compatibleLst[i][2][0],  # "id_third": "2
+        "name_third": third.get_user_info()["name"],
+        "intro_third": third.get_user_info()["introduction"],
+        "email_third": third.get_user_info()["email"]
+    }
+    result.append(res)
+pd.DataFrame(result).to_csv("result.csv", index=False)
